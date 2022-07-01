ca2c2a63da060a5f0ebf3968bcad674dcb09570c
Enes Akbulut <107497616+Enes-Akbulut@users.noreply.github.com>
2022-07-01 10:38:55 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,37 +3,39 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        double number1, number2 ;
+        double number1, number2;
         int select;
         Scanner inp = new Scanner(System.in);
 
         System.out.print("Enter the first number :");
-        number1= inp.nextDouble();
+        number1 = inp.nextDouble();
 
         System.out.print("Enter the second number :");
-        number2= inp.nextDouble();
+        number2 = inp.nextDouble();
 
         System.out.println(" Additon=1 \n Subtraction=2 \n Multiplication=3 \n Divison=4");
         System.out.print("Select the number you want to calculate = ");
-        select= inp.nextInt();
+        select = inp.nextInt();
 
 
-        switch (select){
+        switch (select) {
             case 1:
-                System.out.println(number1+number2);
+                System.out.println("Additon= "+number1 + number2);
                 break;
             case 2:
-                System.out.println(number1-number2);
+                System.out.println("Subtraction= "+number1 - number2);
                 break;
             case 3:
-                System.out.println(number1*number2);
+                System.out.println("Multiplication="+number1 * number2);
                 break;
             case 4:
-                if ((number1==0)||(number2==0)) {
+                if ((number1 == 0) || (number2 == 0)) {
                     System.out.println("You can't divide using 0 (zero)");
-                }else{  System.out.println(number1/number2);}
+                } else {
+                    System.out.println("Divison="+number1 / number2);
+                }
                 break;
-                default:
+            default:
                 System.out.println("You selected  an invalid number. Choose the numbers from 1 to 4.");
         }
 
